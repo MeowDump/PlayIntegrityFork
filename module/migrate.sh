1c631136fa05b77483548df0d83c614be3553e0c
MONA <121730102+MeowDump@users.noreply.github.com>
2026-01-04 11:26:44 +0530

    dropped support for json migration

--- module/migrate.sh
+++ module/migrate.sh
@@ -4,9 +4,9 @@ N="
 ";
 
 case "$1" in
-  -h|--help|help) echo "sh migrate.sh [-f] [-o] [-a] [-p|-j] [in-file] [out-file]"; exit 0;;
+  -h|--help|help) echo "sh migrate.sh [-f] [-o] [-a] [in-file] [out-file]"; exit 0;;
   -i|--install|install) INSTALL=1; shift;;
-  *) echo "custom.pif.prop/.json migration script \
+  *) echo "custom.pif.prop migration script \
     $N  by osm0sis @ xda-developers $N";;
 esac;
 
@@ -18,8 +18,6 @@ until [ -z "$1" -o -f "$1" ]; do
     -f|--force|force) FORCE=1; shift;;
     -o|--override|override) OVERRIDE=1; shift;;
     -a|--advanced|advanced) ADVANCED=1; shift;;
-    -j|--json|json) FORMAT=json; FORCE=1; shift;;
-    -p|--prop|prop) FORMAT=prop; FORCE=1; shift;;
     *) die "Invalid argument/file not found: $1";;
   esac;
 done;
@@ -30,28 +28,15 @@ grep_get_prop() {
 grep_check_prop() {
   grep -q "$1" "$2" && [ "$(grep_get_prop "$1" "$2")" ];
 }
-grep_get_json() {
-  eval set -- "$(cat "$2" | tr -d '\r\n' | grep -m1 -o "$1"'".*' | cut -d: -f2- | sed 's|//|#|g')";
-  echo "$1" | sed -e 's|"|\\\\\\"|g' -e 's|[,}]*$||';
-}
-grep_check_json() {
-  grep -q "$1" "$2" && [ "$(grep_get_json "$1" "$2")" ];
-}
 grep_get_config() {
   local target="$FILE";
   [ -n "$2" ] && target="$2";
-  case $target in
-    *.json) grep_get_json "$1" "$target";;
-    *.prop) grep_get_prop "$1" "$target";;
-  esac;
+  grep_get_prop "$1" "$target";
 }
 grep_check_config() {
   local target="$FILE";
   [ -n "$2" ] && target="$2";
-  case $target in
-    *.json) grep_check_json "$1" "$target";;
-    *.prop) grep_check_prop "$1" "$target";;
-  esac;
+  grep_check_prop "$1" "$target";
 }
 
 if [ -f "$1" ]; then
@@ -66,21 +51,12 @@ fi;
 DIR=$(dirname "$(readlink -f "$DIR")");
 
 if [ -z "$FILE" ]; then
-  for EXT in json prop; do
-    [ -f "$DIR/custom.pif.$EXT" ] && FILE="$DIR/custom.pif.$EXT";
-  done;
+  [ -f "$DIR/custom.pif.prop" ] && FILE="$DIR/custom.pif.prop";
 fi;
 [ -f "$FILE" ] || die "No config file found";
 
-if [ -z "$FORMAT" ]; then
-  case "$FILE" in
-    *.json) FORMAT=json;;
-    *.prop) FORMAT=prop;;
-  esac;
-fi;
-
 OUT="$2";
-[ -z "$OUT" ] && OUT="$DIR/custom.pif.$FORMAT";
+[ -z "$OUT" ] && OUT="$DIR/custom.pif.prop";
 
 grep_check_config api_level && [ ! "$FORCE" ] && die "No migration required";
 
@@ -137,8 +113,8 @@ EOF
 fi;
 
 if [ -z "$SECURITY_PATCH" -o "$SECURITY_PATCH" = "null" ]; then
-  item 'Missing required SECURITY_PATCH field and "*.security_patch" property value found, leaving empty ...';
-  unset SECURITY_PATCH;
+  item 'Missing SECURITY_PATCH value found, setting default ...';
+  SECURITY_PATCH="2025-12-05"
 fi;
 
 if [ -z "$DEVICE_INITIAL_SDK_INT" -o "$DEVICE_INITIAL_SDK_INT" = "null" ]; then
@@ -155,23 +131,21 @@ spoofSignature=0;
 spoofVendingFinger=1;
 spoofVendingSdk=0;
 
+SECURITY_COMMENT=
+
 keep_advanced() {
-  if grep_check_config spoofVendingFinger "$1"; then
-    item "Retaining existing configuration ...";
-    ADVANCED=1;
+  for SETTING in $ADVSETTINGS; do
+    if grep_check_config "$SETTING" "$1"; then
+      item "Retaining existing configuration ...";
+      ADVANCED=1;
 
-    for SETTING in $ADVSETTINGS; do
-      eval grep_check_config $SETTING \"$1\" \&\& \
-        $SETTING=\"$(grep_get_config $SETTING "$1")\";
-    done;
-
-    if grep -qE '#\*.security_patch|//"\*.security_patch"' "$1"; then
-      case $FORMAT in
-        json) SECURITY_COMMENT='//';;
-        prop) SECURITY_COMMENT='#';;
-      esac;
+      for SETTING in $ADVSETTINGS; do
+        eval grep_check_config $SETTING \"$1\" \&\& \
+          $SETTING=\"$(grep_get_config $SETTING "$1")\";
+      done;
+      break;
     fi;
-  fi;
+  done;
 }
 
 if [ -f "$OUT" ]; then
@@ -180,33 +154,37 @@ if [ -f "$OUT" ]; then
   mv -f "$OUT" "$OUT.bak";
 else
   case "$FILE" in
-    *.$FORMAT) ;;
+    *.prop) ;;
     *) keep_advanced "$FILE";;
   esac;
 fi;
 
-[ "$INSTALL" ] || item "Writing fields and properties to updated custom.pif.$FORMAT ...";
+[ "$INSTALL" ] || item "Writing fields and properties to updated custom.pif.prop ...";
 [ "$ADVANCED" ] && item "Adding Advanced Settings entries ...";
 
-case $FORMAT in
-  json) CMNT='  //'; EVALPRE='\ \ \ \ \"'; SECPRE1='    '; SECPRE2='"'; PRE='    "'; MID='": "'; POST='",';;
-  prop) CMNT='#'; MID='=';;
-esac;
-([ "$FORMAT" = "json" ] && echo '{';
-echo "$CMNT Build Fields";
-for FIELD in $ALLFIELDS; do
-  eval echo "$EVALPRE$FIELD\$MID\$$FIELD\$POST";
-done;
-echo "$N$CMNT System Properties";
-echo "$PRE"'*.build.id'"$MID$ID$POST";
-echo "$SECPRE1$SECURITY_COMMENT$SECPRE2"'*.security_patch'"$MID$SECURITY_PATCH$POST";
-[ -z "$VNDK_VERSION" ] || echo "$PRE"'*.vndk.version'"$MID$VNDK_VERSION$POST";
-echo "$PRE"'*api_level'"$MID$DEVICE_INITIAL_SDK_INT$POST";
-if [ "$ADVANCED" ]; then
-  echo "$N$CMNT Advanced Settings";
-  for SETTING in $ADVSETTINGS; do
-    eval echo "$EVALPRE$SETTING\$MID\$$SETTING\$POST";
-  done;
-fi) | sed '$s/,/\n}/' > "$OUT";
+CMNT='#'
+MID='='
+
+{
+  echo "$CMNT Build Fields"
+  for FIELD in $ALLFIELDS; do
+    eval echo "$FIELD$MID\$$FIELD"
+  done
+
+  echo
+  echo "$CMNT System Properties"
+  echo "*.build.id$MID$ID"
+  echo "*.security_patch$MID$SECURITY_PATCH"
+  [ -z "$VNDK_VERSION" ] || echo "*.vndk.version$MID$VNDK_VERSION"
+  echo "*api_level$MID$DEVICE_INITIAL_SDK_INT"
+
+  if [ "$ADVANCED" ]; then
+    echo
+    echo "$CMNT Advanced Settings"
+    for SETTING in $ADVSETTINGS; do
+      eval echo "$SETTING$MID\$$SETTING"
+    done
+  fi
+} > "$OUT"
 
 [ "$INSTALL" ] || cat "$OUT";

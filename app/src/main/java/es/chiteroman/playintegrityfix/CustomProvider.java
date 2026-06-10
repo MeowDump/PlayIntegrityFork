4ede5da20505e781bcfced22f1c15c9967b0b227
MONA <121730102+MeowDump@users.noreply.github.com>
2026-06-10 14:29:55 +0530

    Refactor logging format in getService method

--- app/src/main/java/es/chiteroman/playintegrityfix/CustomProvider.java
+++ app/src/main/java/es/chiteroman/playintegrityfix/CustomProvider.java
@@ -12,9 +12,11 @@ public final class CustomProvider extends Provider {
 
     @Override
     public synchronized Service getService(String type, String algorithm) {
-        if (EntryPoint.getVerboseLogs() > 2) EntryPoint.LOG(String.format("Service: Caller type '%s' with algorithm '%s'", type, algorithm));
-        if (EntryPoint.getSpoofBuildEnabled() > 0) {
-            if (type.equals("KeyStore")) EntryPoint.spoofDevice();
+        if (EntryPoint.getVerboseLogs() > 2) {
+            EntryPoint.LOG("service " + type + " " + algorithm);
+        }
+        if (EntryPoint.getSpoofBuildEnabled() > 0 && "KeyStore".equals(type)) {
+            EntryPoint.spoofDevice();
         }
         return super.getService(type, algorithm);
     }

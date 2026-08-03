5313a8a0983f9cca3ce15ddd967cb2b191ad8563
MONA <121730102+MeowDump@users.noreply.github.com>
2026-08-02 21:34:03 -0700

    Bump SECURITY_PATCH to AUGUST 2026

--- module/migrate.sh
+++ module/migrate.sh
@@ -131,7 +131,7 @@ fi;
 
 if [ -z "$SECURITY_PATCH" -o "$SECURITY_PATCH" = "null" ]; then
 item 'Missing SECURITY_PATCH value found, setting default ...';
-SECURITY_PATCH="2026-07-01"
+SECURITY_PATCH="2026-08-05"
 fi;
 
 if [ -z "$DEVICE_INITIAL_SDK_INT" -o "$DEVICE_INITIAL_SDK_INT" = "null" ]; then
